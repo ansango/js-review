b3ba0fa08ac98eb31451ec6af1b6ca9b45a97f09
ansango <anibalsantosgo@gmail.com>
2020-09-19 14:55:12 +0200

    form validator empty

---
+++ 01 - form-validator/script.js
@@ -0,0 +1,16 @@
+/**
+ * BETTER COMMENTS TEMPLATE
+ * * Titles
+ * @param myParam
+ * ! Review code
+ * ? Questions
+ * TODO: "tasks to todo"
+ * // Deprecated
+ */
+
+/**
+ * * FORM VALIDATOR
+ */
+
+
+ console.log("HELLO FROM FORM VALIDATOR")
